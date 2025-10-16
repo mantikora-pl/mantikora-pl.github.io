3af22c3e92453f1127109fc84cd79f732bf900f3
aleks5230 <aleksjankowiak5230@gmail.com>
2025-10-16 13:54:21 +0200

    MNK-12 Refactor concert list - desktop

--- src/components/ConcertItemM/index.tsx
+++ src/components/ConcertItemM/index.tsx
@@ -1,30 +1,19 @@
 import React from 'react'
 import './style.css'
-import {defaultLang} from "../../helper/translation";
-import concertItem,{Concert, Concert_Old} from "../ConcertItem";
+import {Concert} from "../ConcertItem";
 
 function ConcertItemM({items}:{items: Concert}){
     function unifiedDate(){
         return(items.date.day+' '+items.date.month+' '+items.date.year)
-    }
-    function unifiedDateUS(){
-        return(items.date.month+' '+items.date.day+' '+items.date.year)
     }
 
     return(
         <div className={"concertItemM"}>
             <div className={"concertRowM"}>
                 <div className={"concertLeftM"}>
-                    {defaultLang==="en" &&
                         <div className={"concertDateM"}>
                             <p className={"concertDateTextM"}>{unifiedDate()}</p>
                         </div>
-                    }
-                    {defaultLang!=="en" &&
-                        <div className={"concertDateM"}>
-                            <p className={"concertDateTextM"}>{unifiedDateUS()} </p>
-                        </div>
-                    }
                     <p className={"concertNameM"}>{items.name}</p>
                 </div>
                 <p className={"concertLocationM"}>{items.city}</p>
