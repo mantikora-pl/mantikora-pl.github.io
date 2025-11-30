85963bd26c3dd3b640278934815342a2ad55215f
aleks5230 <aleksjankowiak5230@gmail.com>
2025-11-30 17:00:30 +0100

    [MNK-16] localized and normalized dates in timeline

--- src/components/Timeline/index.tsx
+++ src/components/Timeline/index.tsx
@@ -2,22 +2,34 @@ import React from 'react'
 import {VerticalTimeline,VerticalTimelineElement} from "react-vertical-timeline-component";
 import 'react-vertical-timeline-component/style.min.css';
 import './style.css'
+import {getMonthTranslation,localizeDate} from "../../helper/date";
+import {getLanguage} from "../../helper/translation";
 
 export interface TimelineElement{
     id:number,
     title?:string,
     subtitle?:string,
     description:string,
-    date:string,
-    photo:string
-    photoAlt?:string //TODO: SHOULD BE MANDATORY! NO "?"
+    year:number,
+    month?:number,
+    day?:number,
+    photo:string,
+    photoAlt?:string
+}
+
+function getLocalisedTimelineDate(year:number,month?:number,day?:number){
+    if (!month) return year
+    if (!day) return getMonthTranslation(getLanguage(),month)+" "+year
+    return localizeDate(year,month,day)
 }
 
 export default function Timeline({items}:{items: TimelineElement[]}){
     return <VerticalTimeline>
         {items.map((item,i)=>(
             <VerticalTimelineElement key={i} className="vertical-timeline-element" contentStyle={{background:'#383737'}}>
-                    <h2 className={"timeline-item-title"} style={{color:"white", fontSize:28}}>{item.date}</h2>
+                    <h2 className={"timeline-item-title"} style={{color:"white", fontSize:28}}>
+                        {getLocalisedTimelineDate(item.year,item.month,item.day)}
+                    </h2>
                     {/*<h3 className="timeline-item-subtitle">{item.subtitle}</h3>*/}
                 <div style={{display:"flex"}}>
                     <p className={"timelineItemDetails"}>{item.description}</p>
