691dbd77ccb744b1a86093d08c2c7cd16342784b
aleks5230 <aleksjankowiak5230@gmail.com>
2025-12-28 23:37:00 +0100

    [MNK-47] replaced the feed with a new one

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,14 +1,12 @@
 import React from "react"
 import {AboutUsText} from "../../components/AboutUsText"
+import BeholdWidget from "@behold/react";
 
 export default function Home(){
     return <div className={"innerPage skullBackground"}>
         <AboutUsText/>
-        <div className={"flexJustifyCenter"}>
-            <iframe src="https://app.mirror-app.com/feed-instagram/1b46c940-6f7b-48ed-ac53-7599deed245f/preview"
-                    style={{width:"100%",height:"800px",border:"none"}}
-                    title={"ig"}
-            ></iframe>
+        <div className={"flexJustifyCenter"} id={"instagramWidgetContainer"}>
+            <BeholdWidget feedId="9GO1Tm4URSmW7Uh502yn"/>
         </div>
     </div>
 }
